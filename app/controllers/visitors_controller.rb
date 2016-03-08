b84da4cbcbe18d5034696ff89ff84e830a109030
Kevin Chao <kevin.chao@mail.utoronto.ca>
2016-03-08 01:00:41 -0500

    front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,8 +1,11 @@
 class VisitorsController < ApplicationController
 
   def new
+    #Rails.logger.debug 'DEBUG: entering new method'
     @owner = Owner.new
-    render 'visitors/new'
+    #flash.now[:notice] = 'Welcome home!'
+    #flash.now[:alert] = 'My birthday is coming!'
+    #Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
   end
 
 end
